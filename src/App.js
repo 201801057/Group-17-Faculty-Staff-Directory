e0f61f2d1a27d6342d061d8b7a4ac1816fd75051
unknown <201801057@daiict.ac.in>
2021-04-25 00:50:01 +0530

    About Us Page added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import './App.css';
 import Homepage from "./frontend/pages/Homepage";
 // import ContactUs from "./frontend/pages/ContactUs";
-// import AboutUs from "./frontend/pages/AboutUs";
+import AboutUs from "./frontend/pages/AboutUs";
 import Websitepage from "./frontend/pages/Websitepage";
 // import Login from "./frontend/pages/Login";
 // import CreateAccount from "./frontend/pages/CreateAccount";
@@ -24,8 +24,9 @@ export default function App() {
         <Route path="/web" exact component={Websitepage} />
         <Route path="/Searchpage/" exact component={SearchpageSimple} />
         <Route path="/Searchpage/:searchName" exact component={Searchpage} />
+        <Route path="/AboutUs" exact component={AboutUs} />
+          
         { /* <Route path="/ContactUs" exact component={ContactUs} />
-          <Route path="/AboutUs" exact component={AboutUs} />
           <Route path="/Login" exact component={Login} />
           <Route path="/CreateAccount" exact component={CreateAccount} />
           <Route path="/AboutProf/:searchName" exact component={AboutProf} />
---
+++ src/frontend/pages/AboutUs.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import About from "../components/About";
+import Footer from "../components/Footer";
+
+import "./styles.css"
+
+export default function ContactUs() {
+  return (
+    <div className="AboutUs">
+      <Header></Header>
+      <About></About>
+      <Footer></Footer>
+    </div>
+  );
+}
