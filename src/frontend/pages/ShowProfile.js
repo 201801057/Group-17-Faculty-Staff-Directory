a55ee989bbfd64f93d51bfbd8e23423caf0ec23b
Patil Tushar <patiltushar2101@gmail.com>
2021-05-05 23:28:26 +0530

    final design

--- src/frontend/pages/ShowProfile.js
+++ src/frontend/pages/ShowProfile.js
@@ -4,7 +4,6 @@ import Footer from "../components/Footer";
 import ShowProfile from "../components/ShowProfile";
 import "./styles.css"
 
-
 export default function f() {
     return (
         <div className="Searchpage">
